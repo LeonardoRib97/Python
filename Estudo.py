3bf3e1728b2a10355ca104573cb02fb089af3365
Leonardo José de Carvalho Ribeiro <79900817+LeonardoRib97@users.noreply.github.com>
2026-01-07 20:09:58 -0300

    Add files via upload
    Calculadora interativa completa, com tratamento de erros, validação de operador e  proteção contra divisão por zero

---
+++ Estudo.py
@@ -0,0 +1,53 @@
+
+
+
+
+
+while  True :
+
+    try :
+        num1 = float(input('Digite o primeiro numero \n'))
+        num2 = float(input('Digite o segundo numero \n'))
+        print('Digite o operador (+-/*)')
+        operador = input()
+        resposta = ""
+
+        if operador not in ('+','-','/','*'):
+            print('Operador invalido')
+
+        elif operador == '+':
+            resultado = (num1 + num2)
+            print(resultado)
+
+        elif operador == '-':
+            resultado = (num1 - num2)
+            print(resultado)
+
+        elif operador == '/':
+
+            if  num2 == 0:
+                 print('Nao é possivel fazer divisao por *zero* ')
+            else:
+                resultado = (num1 / num2)
+                print(resultado)
+
+        elif operador == '*':
+            resultado = (num1 * num2)
+            print(resultado)
+
+        while resposta not in ('s', 'n'):
+            resposta = input('Deseja fazer uma nova conta,[S]im ou [N]ao ? ').lower()
+            print('Resposta incorreta, digite apenas (Sim) ou (Nao) ')
+
+        if resposta.startswith('s'):
+            print('OK,Vamos nessa ')
+        elif resposta.startswith('n'):
+            print('Voce finalizou o sistema !!')
+            break
+
+
+    except ValueError :
+        print('Digite apenas numeros válidos !!')
+
+
+
